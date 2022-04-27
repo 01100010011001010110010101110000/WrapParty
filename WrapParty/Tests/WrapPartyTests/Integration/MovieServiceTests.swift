3e5b97657cd38ade4943fecba8aa9b64e71721d3
Tyler Gregory <tdg@tdg.ai>
2022-04-27 16:45:18 -0500

    Setup movie service tests

---
+++ WrapParty/Tests/WrapPartyTests/Integration/MovieServiceTests.swift
@@ -0,0 +1,34 @@
+// Copyright (C) 2022 Tyler Gregory (@01100010011001010110010101110000)
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@testable import WrapParty
+import XCTest
+
+final class MoviceServiceIntegrationTests: XCTestCase {
+  // MARK: Internal
+
+  func testGetDetails() async throws {
+    let movieId = 680
+    let movie = try await Self.client.details(for: movieId)
+
+    XCTAssert(movie.id == movieId)
+  }
+
+  // MARK: Private
+
+  private static let client: MovieService = {
+    let config = DefaultConfiguration()
+    return MovieService(dataLoader: config.loader, tokenManager: TokenManager(token: config.apiToken))
+  }()
+}

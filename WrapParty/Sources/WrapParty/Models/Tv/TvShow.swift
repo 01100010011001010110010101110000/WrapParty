1f7376e1e2db859dab2a4e04686141e55455c740
Tyler Gregory <tdg@tdg.ai>
2022-05-17 00:06:33 -0500

    Add TV show appendables

--- WrapParty/Sources/WrapParty/Models/Tv/TvShow.swift
+++ WrapParty/Sources/WrapParty/Models/Tv/TvShow.swift
@@ -19,16 +19,25 @@ import Foundation
 public struct TvShow: Codable {
   // MARK: Lifecycle
 
-  public init(adult: Bool, backdropPath: URL?, createdBy: [CreatedBy], episodeRunTime: [Int], firstAirDate: String?, genres: [Genre], homepage: String, id: Int, inProduction: Bool, languages: [String], lastAirDate: String?, lastEpisodeToAir: RecentEpisode?, name: String, networks: [Network], nextEpisodeToAir: RecentEpisode?, numberOfEpisodes: Int, numberOfSeasons: Int, originCountry: [String], originalLanguage: String, originalName: String, overview: String, popularity: Double, posterPath: URL?, productionCompanies: [ProductionCompany], productionCountries: [ProductionCountry], seasons: [Season], spokenLanguages: [SpokenLanguage], status: String, tagline: String, type: String, voteAverage: Double, voteCount: Int) {
+  public init(adult: Bool, aggregateCredits: TvAggregateCredits?, alternativeTitles: Results<AlternativeTitle>?, backdropPath: URL?, changes: MediaChanges?, contentRatings: Results<TvContentRating>?, createdBy: [CreatedBy], credits: Credits?, episodeGroups: Results<TvEpisodeGroup>?, episodeRunTime: [Int], externalIds: ExternalIds?, firstAirDate: String?, genres: [Genre], homepage: String, id: Int, images: MediaImages?, inProduction: Bool, keywords: Results<Keyword>?, languages: [String], lastAirDate: String?, lastEpisodeToAir: RecentEpisode?, name: String, networks: [Network], nextEpisodeToAir: RecentEpisode?, numberOfEpisodes: Int, numberOfSeasons: Int, originCountry: [String], originalLanguage: String, originalName: String, overview: String, popularity: Double, posterPath: URL?, productionCompanies: [ProductionCompany], productionCountries: [ProductionCountry], recommendations: ResultPage<TvListResult>?, reviews: ResultPage<Review>?, screenedTheatrically: Results<TvScreenedEpisode>?, seasons: [Season], similar: ResultPage<TvListResult>?, spokenLanguages: [SpokenLanguage], status: String, tagline: String, translations: MediaTranslations?, type: String, videos: Results<MediaVideo>?, voteAverage: Double, voteCount: Int, watchProviders: WatchProviders?) {
     self.adult = adult
+    self.aggregateCredits = aggregateCredits
+    self.alternativeTitles = alternativeTitles
     self.backdropPath = backdropPath
+    self.changes = changes
+    self.contentRatings = contentRatings
     self.createdBy = createdBy
+    self.credits = credits
+    self.episodeGroups = episodeGroups
     self.episodeRunTime = episodeRunTime
+    self.externalIds = externalIds
     self.firstAirDate = firstAirDate
     self.genres = genres
     self.homepage = homepage
     self.id = id
+    self.images = images
     self.inProduction = inProduction
+    self.keywords = keywords
     self.languages = languages
     self.lastAirDate = lastAirDate
     self.lastEpisodeToAir = lastEpisodeToAir
@@ -45,26 +54,42 @@ public struct TvShow: Codable {
     self.posterPath = posterPath
     self.productionCompanies = productionCompanies
     self.productionCountries = productionCountries
+    self.recommendations = recommendations
+    self.reviews = reviews
+    self.screenedTheatrically = screenedTheatrically
     self.seasons = seasons
+    self.similar = similar
     self.spokenLanguages = spokenLanguages
     self.status = status
     self.tagline = tagline
+    self.translations = translations
     self.type = type
+    self.videos = videos
     self.voteAverage = voteAverage
     self.voteCount = voteCount
+    self.watchProviders = watchProviders
   }
 
   // MARK: Public
 
   public let adult: Bool
+  public let aggregateCredits: TvAggregateCredits?
+  public let alternativeTitles: Results<AlternativeTitle>?
   public let backdropPath: URL?
+  public let changes: MediaChanges?
+  public let contentRatings: Results<TvContentRating>?
   public let createdBy: [CreatedBy]
+  public let credits: Credits?
+  public let episodeGroups: Results<TvEpisodeGroup>?
   public let episodeRunTime: [Int]
+  public let externalIds: ExternalIds?
   public let firstAirDate: String?
   public let genres: [Genre]
   public let homepage: String
   public let id: Int
+  public let images: MediaImages?
   public let inProduction: Bool
+  public let keywords: Results<Keyword>?
   public let languages: [String]
   public let lastAirDate: String?
   public let lastEpisodeToAir: RecentEpisode?
@@ -81,26 +106,42 @@ public struct TvShow: Codable {
   public let posterPath: URL?
   public let productionCompanies: [ProductionCompany]
   public let productionCountries: [ProductionCountry]
+  public let recommendations: ResultPage<TvListResult>?
+  public let reviews: ResultPage<Review>?
+  public let screenedTheatrically: Results<TvScreenedEpisode>?
   public let seasons: [Season]
+  public let similar: ResultPage<TvListResult>?
   public let spokenLanguages: [SpokenLanguage]
   public let status: String
   public let tagline: String
+  public let translations: MediaTranslations?
   public let type: String
+  public let videos: Results<MediaVideo>?
   public let voteAverage: Double
   public let voteCount: Int
+  public let watchProviders: WatchProviders?
 
   // MARK: Internal
 
   enum CodingKeys: String, CodingKey {
     case adult
+    case aggregateCredits = "aggregate_credits"
+    case alternativeTitles = "alternative_titles"
     case backdropPath = "backdrop_path"
+    case changes
+    case contentRatings = "content_ratings"
     case createdBy = "created_by"
+    case credits
+    case episodeGroups = "episode_groups"
     case episodeRunTime = "episode_run_time"
+    case externalIds = "external_ids"
     case firstAirDate = "first_air_date"
     case genres
     case homepage
     case id
+    case images
     case inProduction = "in_production"
+    case keywords
     case languages
     case lastAirDate = "last_air_date"
     case lastEpisodeToAir = "last_episode_to_air"
@@ -117,13 +158,20 @@ public struct TvShow: Codable {
     case posterPath = "poster_path"
     case productionCompanies = "production_companies"
     case productionCountries = "production_countries"
+    case recommendations
+    case reviews
+    case screenedTheatrically = "screened_theatrically"
     case seasons
+    case similar
     case spokenLanguages = "spoken_languages"
     case status
     case tagline
+    case translations
     case type
+    case videos
     case voteAverage = "vote_average"
     case voteCount = "vote_count"
+    case watchProviders = "watch/providers"
   }
 }
 

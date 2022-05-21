18b88ab26429111eb749f534633d2d786fe28ff7
Tyler Gregory <tdg@tdg.ai>
2022-05-20 21:48:31 -0500

    Add config service

---
+++ WrapParty/Tests/WrapPartyTests/Integration/ConfigurationServiceTests.swift
@@ -0,0 +1,35 @@
+// Copyright (C) 2022 Tyler Gregory (@01100010011001010110010101110000)
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@testable import WrapParty
+
+import Foundation
+import XCTest
+
+final class ConfigurationServiceIntegrationTests: XCTestCase {
+  // MARK: Internal
+
+  func testGetConfiguration() async throws {
+    let configuration = try await Self.service.configuration()
+
+    XCTAssertFalse(configuration.changeKeys.isEmpty)
+  }
+
+  // MARK: Private
+
+  private static let service: ConfigurationService = {
+    let config = DefaultConfiguration()
+    return ConfigurationService(dataLoader: config.loader, logger: config.logger, tokenManager: TokenManager(token: config.apiToken))
+  }()
+}
